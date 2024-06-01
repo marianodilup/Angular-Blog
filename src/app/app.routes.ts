a972f31ece6249eba09e19129117fcb561f60bcf
marianodilup <mangypul@gmail.com>
2024-06-02 00:27:38 +0200

    new updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CreatePostComponent } from './components/create-post/create-post.component';
+import { DetailPostComponent } from './components/detail-post/detail-post.component';
+import { PostListComponent } from './components/post-list/post-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'create-post', component: CreatePostComponent },
+  { path: 'detail-post', component: DetailPostComponent },
+  { path: 'post-list', component: PostListComponent },
+];
---
+++ src/app/components/detail-post/detail-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-post.component.html',
+  styleUrl: './detail-post.component.css'
+})
+export class DetailPostComponent {
+
+}
