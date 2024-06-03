007fbed321ab6d27dbb3460f14d2447e5a4a5583
marianodilup <mangypul@gmail.com>
2024-06-03 22:28:37 +0200

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { DetailPostComponent } from './components/detail-post/detail-post.compon
 import { PostListComponent } from './components/post-list/post-list.component';
 
 export const routes: Routes = [
-  { path: 'create-post', component: CreatePostComponent },
-  { path: 'detail-post', component: DetailPostComponent },
-  { path: 'post-list', component: PostListComponent },
+  { path: 'createPost', component: CreatePostComponent },
+  { path: 'detailPost', component: DetailPostComponent },
+  { path: 'postList', component: PostListComponent },
 ];
--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
+import Post from '../models/models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BlogService {
-  constructor() {}
+  post: Post[]; //interface
+
+  constructor() {
+    this.post = [
+      //poner aqui los campos del objeto de la interface. y despues hay que utilizar el servicio en el componente donde queremos utilizar la lista, poner public etc... en el constructor post list.
+    ];
+  }
 
   getAll() {}
 }
